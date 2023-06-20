7eed480c81a10fa9f01ecdbb115ebb80eb98f206
Akkhrawin <132987465+Akkhrawin@users.noreply.github.com>
2023-06-20 08:42:10 +0000

    sumit

--- .ccls-cache/@home@runner@CalculateTime-AKKHRAWINNAIR/Time.h
+++ .ccls-cache/@home@runner@CalculateTime-AKKHRAWINNAIR/Time.h
@@ -1,6 +1,36 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
 
+void getTime (int &h,int &m){
+ cout<<endl<<" input hour & min:";
+cin>>h>>m;
+}
+int subtract(int h1,int m1,int h2,int m2){
+  int hour,minute;
+  hour = h2-h1; 
+  minute = m2-m1;
+  if (hour>=0&&minute>=0){
+    return hour;
+  }
+  else if (hour>=0&&minute<0) {
+    return hour-1;
+  }
+  else if (hour<0&&minute>=0) {
+    return hour+24;
+  }
+  else if (hour<0&&minute<0) {
+    return hour+24-1;
+  }
+}
 
-void getTime(int h,int m){
-  
-  
+int subtract(int k1,int k2){
+  int min;
+  min= k2-k1;
+  if(min<0){
+    return min+60;
+  }
+  else{
+    return min;
+  }
 }
